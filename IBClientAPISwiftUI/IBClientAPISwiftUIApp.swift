c4a639424c7f12f0b9cb71afc59c6f8305332e49
Daniil Poletaev <poletdan@fit.cvut.cz>
2022-03-18 23:02:30 +0100

    Initial Commit

---
+++ IBClientAPISwiftUI/IBClientAPISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBClientAPISwiftUIApp.swift
+//  IBClientAPISwiftUI
+//
+//  Created by Danil Poletaev on 18.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct IBClientAPISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
